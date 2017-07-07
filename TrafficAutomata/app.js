584f447c787379df3477c6a2f680027858e20b97
Benji Altman <benji@altmansoftwaredesign.com>
2017-07-07 08:00:04 -0500

    minor changes

--- TrafficAutomata/app.js
+++ TrafficAutomata/app.js
@@ -20,7 +20,7 @@ const DENSITY = 0.2;
  *
  * @param {String|Number} x
  * @param {String|Number} y
- * @returns {Array} this
+ * @returns {Object} this
  */
 Object.prototype.swap = function (x, y) {
 	var b = this[x];
@@ -170,17 +170,6 @@ function setSpeed(current, next, vMax, pFault, pSlow, roadLength) {
 	}
 }
 
-
-var history = [INITIALIZE()];
-while (history.length < 25) {
-	history.push(Run(history[history.length - 1], vMAX, pFAULT, pSLOW, ROAD_LENGTH, 1));
-}
-history.push('jumping 1000 steps now');
-history.push(Run(history[history.length - 2], vMAX, pFAULT, pSLOW, ROAD_LENGTH, 1000));
-while (history.length < 51) {
-	history.push(Run(history[history.length - 1], vMAX, pFAULT, pSLOW, ROAD_LENGTH, 1));
-}
-print(history, ROAD_LENGTH);
 
 
 /**
